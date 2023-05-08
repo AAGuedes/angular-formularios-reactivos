c5182b91efe11635ebdb1737249dbe9b72d2e26f
AAGuedes <airam.a.g.85@gmail.com>
2023-05-08 10:54:51 +0100

    Forms Validator - Validador incluido en Angular

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -10,9 +10,9 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class BasicPageComponent {
 
   public myForm: FormGroup = this.formBuilder.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: [ '', [ Validators.required, Validators.minLength(3) ] ],
+    price: [ 0, [ Validators.required, Validators.min(0) ] ],
+    inStorage: [ 0, [ Validators.required, Validators.min(0) ] ],
   });
 
   constructor(
@@ -20,6 +20,7 @@ export class BasicPageComponent {
   ) {}
 
   onSave(): void {
+    if(this.myForm.invalid) return;
     console.log(this.myForm.value);
   }
 
