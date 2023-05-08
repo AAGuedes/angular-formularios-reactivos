68eefb91aa86a422925ecc660c15e349259bc1f5
AAGuedes <airam.a.g.85@gmail.com>
2023-05-08 12:19:53 +0100

    Mostrar errores de validación en pantalla

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -26,12 +26,14 @@ export class BasicPageComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.myForm.reset(rtx5090);
+    // this.myForm.reset(rtx5090);
   }
 
   onSave(): void {
-    if(this.myForm.invalid) return;
-    console.log(this.myForm.value);
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     this.myForm.reset({ price: 0, inStorage: 0 });
   }
 
