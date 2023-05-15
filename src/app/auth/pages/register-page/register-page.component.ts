b6178826e4b46245ee461e969073f857b57f7d11
AAGuedes <airam.a.g.85@gmail.com>
2023-05-15 08:18:58 +0100

    Validaciones de Email y Patrones

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { candBeStrider } from 'src/app/shared/validators/validators';
+import * as customValidators from 'src/app/shared/validators/validators';
 
 
 @Component({
@@ -10,9 +10,9 @@ import { candBeStrider } from 'src/app/shared/validators/validators';
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.formBuilder.group({
-    name: ['', [ Validators.required ]],
-    email: ['', [ Validators.required ]],
-    username: ['', [ Validators.required, candBeStrider ]],
+    name: ['', [ Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern) ]],
+    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern) ]],
+    username: ['', [ Validators.required, customValidators.candBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]]
   })
