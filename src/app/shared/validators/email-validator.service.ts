7fa3b0591d49dab842656dceeb89c4d36931ed8f
AAGuedes <airam.a.g.85@gmail.com>
2023-05-15 11:41:31 +0100

    Elaborar el validador de correo

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
 
-import { Observable, delay, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -13,13 +13,41 @@ export class EmailValidatorService implements AsyncValidator {
 
     const email = control.value;
 
-    console.log({ email })
+    const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
 
-    return of({
-      emailTaken: true
-    }).pipe(
-      delay(2000)
-    )
+      console.log({ email });
+
+      if(email == 'john.doe@mail.com') {
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
+    });
+
+    return httpCallObservable;
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+  //   const email = control.value;
+
+  //   console.log({ email })
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   )
+  // }
+
+  // return this.http.get<any[]>(`http://localhost:3000/users?q=${email}`)
+  //   .pipe(
+  //     map(resp => {
+  //       return (resp.length === 0) ? null : {emailTaken: true}
+  //     })
+  //   );
 
 }
