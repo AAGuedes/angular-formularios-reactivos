5ecf7c4fa138a0e4b648c726e5b6dfa83df8e1a8
AAGuedes <airam.a.g.85@gmail.com>
2023-05-15 07:39:32 +0100

    Formulario de Registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]]
+  })
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  isValidField(field:string) {
+
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
